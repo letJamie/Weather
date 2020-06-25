b5d0f1d8d6d1da9c9d55068a8e399e4250c84a2e
letJamie <stillbut@naver.com>
2020-06-26 00:03:16 +0900

    Fetch weather data from url

--- Weather/Model/WeatherManager.swift
+++ Weather/Model/WeatherManager.swift
@@ -10,11 +10,41 @@ import Foundation
 
  struct WeatherManager {
     
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=ffa90d9279c5c55c5634e893ee47a6a1&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=ffa90d9279c5c55c5634e893ee47a6a1&units=metric"
     
     func fatchWeather(cityName: String) {
         
         let urlString = "\(weatherURL)&q=\(cityName)"
+        
+        performRequest(urlString: urlString)
     }
     
+    func performRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url, completionHandler: handle(data: urlResponse: error:) )
+                
+            task.resume()
+        }
+        
+        
+    }
+    
+    func handle(data: Data?, urlResponse: URLResponse?, error: Error?) {
+        
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+            
+        }
+    }
  }
